f6a4fe121dae756a34c4b03efe69f6c1894e4bb2
Eli Mouesca <elias.mouesca@gmail.com>
2026-09-06 08:48:00 -0300

    Rename package to com.elicapo.launcher

---
+++ app/src/main/java/com/elicapo/launcher/helper/FakeHomeActivity.kt
@@ -0,0 +1,12 @@
+package com.elicapo.launcher.helper
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.elicapo.launcher.R
+
+class FakeHomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fake_home)
+    }
+}
